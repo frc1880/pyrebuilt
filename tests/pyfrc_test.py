b1ce873427ebbaebebcffcdd48634c409787d394
James Ward <james@thedropbears.org.au>
2026-01-17 22:15:00 -0500

    Add tests

--- tests/pyfrc_test.py
+++ tests/pyfrc_test.py
@@ -2,3 +2,15 @@
 This test module imports tests that come with pyfrc, and can be used
 to test basic functionality of just about any robot.
 """
+
+# NB: autonomous is tested separately in autonomous_test.py
+from pyfrc.tests import (
+    test_disabled,
+    test_operator_control,
+)
+
+# Make pyflakes happy about our imports.
+__all__ = (
+    "test_disabled",
+    "test_operator_control",
+)
