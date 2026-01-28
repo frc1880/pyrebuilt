7f58fb867ec226e7bcf637ba4c66217a3bbff078
James Ward <james@thedropbears.org.au>
2026-01-28 11:10:44 -0500

    Add ids.py to track component IDs

---
+++ ids.py
@@ -0,0 +1,51 @@
+import enum
+
+
+@enum.unique
+class TalonId(enum.IntEnum):
+    """CAN ID for CTRE Talon motor controllers (e.g. Talon FX, Talon SRX)."""
+
+    pass
+
+
+@enum.unique
+class CancoderId(enum.IntEnum):
+    """CAN ID for CTRE CANcoder."""
+
+    pass
+
+
+@enum.unique
+class SparkId(enum.IntEnum):
+    """CAN ID for REV SPARK motor controllers (Spark Max, Spark Flex)."""
+
+    pass
+
+
+@enum.unique
+class DioChannel(enum.IntEnum):
+    """roboRIO Digital I/O channel number."""
+
+    pass
+
+
+@enum.unique
+class PwmChannel(enum.IntEnum):
+    """roboRIO PWM output channel number."""
+
+    pass
+
+
+@enum.unique
+class RioSerialNumber(enum.StrEnum):
+    """roboRIO serial number"""
+
+    TEST_BOT = "12345678"
+    COMP_BOT = "87654321"
+
+
+@enum.unique
+class AnalogChannel(enum.IntEnum):
+    """roboRIO Analog input channel number"""
+
+    pass
