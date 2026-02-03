ac659b08df54ade1e80446ef87e990b22c069c92
Jay Planas <jayden.planas@eastharlemscholars.org>
2026-02-03 16:06:42 -0500

    Update shooter.py

--- components/shooter.py
+++ components/shooter.py
@@ -8,18 +8,18 @@ class Shooter:
     shooter_speed = tunable(0.25)
 
     def __init__(self) -> None:
-        self._should_shooter = False
+        self._should_shoot = False
         self._shooter_motor = phoenix6.hardware.TalonFX(
             ids.TalonId.SHOOTER_MOTOR, ids.CanbusId.SHOOTER
         )
 
     def shoot(self) -> None:
-        self._should_shooter = True
+        self._should_shoot = True
 
     def execute(self) -> None:
-        if self._should_shooter:
+        if self._should_shoot:
             # spin shooter motor
             self._shooter_motor.set(self.shooter_speed)
         else:
             self._shooter_motor.stopMotor()
-        self._should_shooter = False
+        self._should_shoot = False
