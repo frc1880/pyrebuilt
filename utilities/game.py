04600f64329482946ad6fb7b08b2c64179bea88b
James Ward <james@thedropbears.org.au>
2026-02-03 23:38:58 -0500

    Set default starting positions

--- utilities/game.py
+++ utilities/game.py
@@ -1,5 +1,29 @@
+import robotpy_apriltag
 import wpilib
+from wpimath.geometry import Pose2d, Rotation2d, Translation2d
 
 
 def is_blue() -> bool:
     return wpilib.DriverStation.getAlliance() == wpilib.DriverStation.Alliance.kBlue
+
+
+apriltag_layout = robotpy_apriltag.AprilTagFieldLayout.loadField(
+    robotpy_apriltag.AprilTagField.k2026RebuiltWelded
+)
+
+
+def field_flip_pose2d(p: Pose2d):
+    return Pose2d(
+        field_flip_translation2d(p.translation()),
+        field_flip_rotation2d(p.rotation()),
+    )
+
+
+def field_flip_rotation2d(r: Rotation2d):
+    return Rotation2d(-r.cos(), r.sin())
+
+
+def field_flip_translation2d(t: Translation2d):
+    return Translation2d(
+        apriltag_layout.getFieldLength() - t.x, apriltag_layout.getFieldWidth() - t.y
+    )
