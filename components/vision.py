87be5e7f2ddcce26f8a73e916cbd07604c447897
Some-thing246 <danieldk2030@gmail.com>
2026-02-06 19:18:12 -0500

    Fixed Drivetrain Calling and Added Turret camera

--- components/vision.py
+++ components/vision.py
@@ -16,7 +16,7 @@ class Vision:
 
     def __init__(self, camera_name: str, transform: Transform3d) -> None:
         # Instantiate the camera/photonvision
-        self.cam = PhotonCamera("Front Camera")
+        self.frontcam = PhotonCamera("Front Camera")
         kRobotToCam = wpimath.geometry.Transform3d(
             wpimath.geometry.Translation3d(0.5, 0.0, 0.5),
             wpimath.geometry.Rotation3d.fromDegrees(0.0, -30.0, 0.0),
@@ -25,12 +25,21 @@ class Vision:
             AprilTagFieldLayout.loadField(AprilTagField.kDefaultField),
             kRobotToCam,
         )
-
+        self.turretcam= = PhotonCamera("Turret Camera")
 
 
     def execute(self) -> None:
             # Get any observations from photonvision and add them to the drivetrain
-        for result in self.cam.getAllUnreadResults():
+        for result in self.frontcam.getAllUnreadResults():
             camEstPose = self.camPoseEst.estimateCoprocMultiTagPose(result)
             if camEstPose is None:
                 camEstPose = self.camPoseEst.estimateLowestAmbiguityPose(result)
+            self.drivetrain.addVisionPoseEstimate(
+                camEstPose.estimatedPose, camEstPose.timestampSeconds
+            )
+
+        results = self.turretcam.getLatestResult()
+        if (result.hasTargets()):
+            best= result.getBestTarget()
+            # check if scoring april tag
+            # update yaw from scoring tag
