21394acfb71322319338cadaa1042da1ca2c1083
James Ward <james@thedropbears.org.au>
2026-03-05 16:29:19 -0500

    WIP: deploy code

--- components/intake.py
+++ components/intake.py
@@ -1,5 +1,6 @@
 import phoenix6
 from magicbot import tunable
+from phoenix6 import configs, controls
 
 import ids
 
@@ -7,22 +8,69 @@ import ids
 class Intake:
     intake_speed = tunable(0.5)
 
+    deployed_position = tunable(0.0)
+
     def __init__(self) -> None:
         self._should_intake = False
-        self._intake_roller_motor = phoenix6.hardware.TalonFX(
+        self._roller_motor = phoenix6.hardware.TalonFX(
             ids.TalonId.INTAKE_ROLLER_MOTOR, ids.CanbusId.INTAKE
         )
-        self._intake_deploy_motor = phoenix6.hardware.TalonFX(
+        self._deploy_motor = phoenix6.hardware.TalonFX(
             ids.TalonId.INTAKE_DEPLOY_MOTOR, ids.CanbusId.INTAKE
         )
+
+        deploy_pid_cfg = configs.Slot0Configs()
+        # TODO tune these values
+        deploy_pid_cfg.k_p = 1.0  # 1 rev error will output 1V
+        deploy_pid_cfg.k_i = 0.0
+        deploy_pid_cfg.k_d = 0.0
+        self._deploy_motor.configurator.apply(
+            configs.TalonFXConfiguration().with_slot0(deploy_pid_cfg)
+        )
+
+        # Variables used for zeroing against hard stop
+        self._desired_intake_position = 0.0
+        self._prev_intake_angle = 0.0
+        self._initialized = False
 
     def intake(self) -> None:
         self._should_intake = True
 
+    def deploy(self) -> None:
+        self._desired_intake_position = self.deployed_position
+
+    def retract(self) -> None:
+        self._desired_intake_position = 0.0
+
     def execute(self) -> None:
+        if not self._initialized:
+            # Drive the motor very slowly towards the hard stop
+            # Check to see if we are still moving/current spike
+            # If we are stopped, reset the encoder value and put the motor in closed loop mode
+            # TODO Is this output too small?
+            self._deploy_motor.set_control(controls.DutyCycleOut(0.05))
+
+            current = self._deploy_motor.get_stator_current().value
+            angle = self._deploy_motor.get_position().value
+
+            # TODO Check 1 degree threshold is okay
+            # TODO Maybe use current as well - we expect a spike when stalled, but it will also spike on start
+            # TODO Check 2A current threshold is okay
+            if abs(angle - self._prev_intake_angle) < 0.5 and current > 4.0:
+                self._deploy_motor.set_position(0.0)
+                self._desired_intake_position = 0.0
+                self._initialized = True
+            else:
+                self._prev_intake_angle = angle
+                return  # we can't shoot until we are ready
+
+        self._deploy_motor.set_control(
+            controls.PositionVoltage(self._desired_intake_position)
+        )
+
         if self._should_intake:
             # Spin the intake motor
-            self._intake_roller_motor.set(self.intake_speed)
+            self._roller_motor.set(self.intake_speed)
         else:
-            self._intake_roller_motor.stopMotor()
+            self._roller_motor.stopMotor()
         self._should_intake = False
