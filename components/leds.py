de7150f6e89c73a857422c1a72030c963bd1bfb0
James Ward <james@thedropbears.org.au>
2026-03-11 11:14:05 -0400

    Skeleton for pre-game lights for drive team

--- components/leds.py
+++ components/leds.py
@@ -7,6 +7,7 @@ from wpilib import DriverStation
 from components.ballistics import Ballistics
 from ids import CanbusId, CandleId
 from utilities.game import is_hub_active, time_to_hub_active
+from wpimath.geometry import Transform2d
 
 
 class Leds:
@@ -53,6 +54,39 @@ class Leds:
         self._pattern = (
             self._flashing(self.RED) if should_flash else self._solid(self.RED)
         )
+
+    def missing_vision(self) -> None:
+        pass
+
+    def missing_auto(self) -> None:
+        pass
+
+    def wrong_start(self, error: Transform2d):
+        # Light up various parts of the robot to show the direction to move it
+        if error.rotation().radians() > 0:
+            # Negative z rotation required
+            pass
+        elif error.rotation().radians() < 0:
+            # Positive z rotation required
+            pass
+        else:
+            # Rotation is okay, so give feedback on position
+            # NB these need to work together, eg move forward and move left simultaneously
+            if error.translation().x > 0:
+                # Move back required
+                pass
+            elif error.translation().x < 0:
+                # Move forward required
+                pass
+            if error.translation().y > 0:
+                # Move right required
+                pass
+            elif error.translation().y < 0:
+                # Move left required
+                pass
+
+    def disabled(self) -> None:
+        pass
 
     def execute(self) -> None:
         if not DriverStation.isTestEnabled():
