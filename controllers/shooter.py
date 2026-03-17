243a634a056ee996aa40e83591c3b13457ae2065
James Ward <james@thedropbears.org.au>
2026-03-16 23:47:44 -0400

    Pass towards alliance wall if not in alliance zone

--- controllers/shooter.py
+++ controllers/shooter.py
@@ -1,3 +1,5 @@
+import math
+
 from magicbot import StateMachine, state
 
 from components.ballistics import Ballistics
@@ -11,14 +13,26 @@ class ShooterController(StateMachine):
     drivetrain: Drivetrain
     indexer: Indexer
 
+    def _heading(self) -> float:
+        # If we are in our alliance zone, aim at the hub
+        # Otherwise fire back at the alliance wall as a passing shot
+        if game.is_in_alliance_zone(self.drivetrain.pose()):
+            return positions.shooter_to_hub(self.drivetrain.pose()).radians()
+        else:
+            return math.radians(-90.0) if game.is_red() else math.radians(90.0)
+
+    def _can_shoot(self) -> bool:
+        # We can shoot if we are in our zone and the hub is active,
+        # or we are outside our zone at any time
+        in_zone = positions.is_in_alliance_zone(self.drivetrain.pose())
+        return (game.is_hub_active() and in_zone) or not in_zone
+
     @state(first=True)
     def aligning(self) -> None:
-        if not self.ballistics.is_within_range():
+        if not self._can_shoot():
             return
         # Point at the target
-        self.drivetrain.track_heading(
-            positions.shooter_to_hub(self.drivetrain.pose()).radians()
-        )
+        self.drivetrain.track_heading(self._heading())
         if self.drivetrain.is_aligned():
             self.next_state("shooting")
 
@@ -26,14 +40,8 @@ class ShooterController(StateMachine):
     def shooting(self) -> None:
         # Check to see that we are still aligned with the goal
         # This is important if we are being defended
-        self.drivetrain.track_heading(
-            positions.shooter_to_hub(self.drivetrain.pose()).radians()
-        )
-        if (
-            not self.drivetrain.is_aligned()
-            or not self.ballistics.is_within_range()
-            or not game.is_hub_active()
-        ):
+        self.drivetrain.track_heading(self._heading())
+        if not (self._can_shoot() and self.drivetrain.is_aligned()):
             self.next_state_now("aligning")
         else:
             # We are still aligned, so keep shooting
