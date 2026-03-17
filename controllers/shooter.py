759eb1d67d5137109a1c64b6f31faa7fb8693d6d
James Ward <james@thedropbears.org.au>
2026-03-16 23:24:49 -0400

    Refactor to allow any heading to track

--- controllers/shooter.py
+++ controllers/shooter.py
@@ -3,7 +3,7 @@ from magicbot import StateMachine, state
 from components.ballistics import Ballistics
 from components.drivetrain import Drivetrain
 from components.indexer import Indexer
-from utilities import game
+from utilities import game, positions
 
 
 class ShooterController(StateMachine):
@@ -16,17 +16,21 @@ class ShooterController(StateMachine):
         if not self.ballistics.is_within_range():
             return
         # Point at the target
-        self.drivetrain.track_hub()
-        if self.drivetrain.is_aligned_with_hub():
+        self.drivetrain.track_heading(
+            positions.shooter_to_hub(self.drivetrain.pose()).radians()
+        )
+        if self.drivetrain.is_aligned():
             self.next_state("shooting")
 
     @state
     def shooting(self) -> None:
         # Check to see that we are still aligned with the goal
         # This is important if we are being defended
-        self.drivetrain.track_hub()
+        self.drivetrain.track_heading(
+            positions.shooter_to_hub(self.drivetrain.pose()).radians()
+        )
         if (
-            not self.drivetrain.is_aligned_with_hub()
+            not self.drivetrain.is_aligned()
             or not self.ballistics.is_within_range()
             or not game.is_hub_active()
         ):
