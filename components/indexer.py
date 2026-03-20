42c2b9ba1163621e5710f3a022349a09dc6bbdcf
James Ward <james@thedropbears.org.au>
2026-03-20 12:28:44 -0400

    Backdrive spindexer

--- components/indexer.py
+++ components/indexer.py
@@ -1,5 +1,5 @@
 import phoenix6
-from magicbot import tunable
+from magicbot import tunable, will_reset_to
 from phoenix6 import configs, signals
 
 import ids
@@ -7,9 +7,10 @@ import ids
 
 class Indexer:
     indexer_speed_rotation = tunable(1.0)
+    _should_feed = will_reset_to(False)
+    _should_backdrive = will_reset_to(False)
 
     def __init__(self) -> None:
-        self._should_feed = False
         self._indexer_rotation_motor = phoenix6.hardware.TalonFX(
             ids.TalonId.INDEXER_ROTATION_MOTOR, ids.CanbusId.INDEXER
         )
@@ -26,12 +27,17 @@ class Indexer:
     def feed(self) -> None:
         self._should_feed = True
 
+    def backdrive(self) -> None:
+        self._should_backdrive = True
+
     def execute(self) -> None:
         if self._should_feed:
             # Spin the index motor
             self._indexer_rotation_motor.set(self.indexer_speed_rotation)
             self._injector_motor.set(1.0)
+        elif self._should_backdrive:
+            self._injector_motor.set(-1.0)
+            self._indexer_rotation_motor.set(-0.25)
         else:
             self._indexer_rotation_motor.stopMotor()
             self._injector_motor.stopMotor()
-        self._should_feed = False
