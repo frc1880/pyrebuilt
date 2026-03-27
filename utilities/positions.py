a9e10d4effcb1ccca55e32ea649f26c0fc4808e4
damian <payanodamian18@gmail.com>
2026-03-27 11:58:25 -0400

    Fixed Shooting Position

--- utilities/positions.py
+++ utilities/positions.py
@@ -45,8 +45,7 @@ def shooter_to_hub(robot_pose: Pose2d) -> Rotation2d:
     # The desired heading is for the shooter where it is now, and it will move as we rotate
     # This will still converge because we keep updating the setpoint as the shooter moves around
     desired_heading = (
-        math.atan2(hub.y - shooter_position.y, hub.x - shooter_position.x)
-        - math.pi / 2.0
+        math.atan2(hub.y - shooter_position.y, hub.x - shooter_position.x) - math.pi
     )  # Shooter is at rear of robot facing on the +ve y axis
 
     return Rotation2d(desired_heading)
