39152bd37cee1662685181ffd90c898921c913fc
damian <payanodamian18@gmail.com>
2026-04-07 10:05:47 -0400

    Deleted vision coordinator

--- robot.py
+++ robot.py
@@ -21,7 +21,6 @@ from components.intake import Intake
 from components.leds import Leds
 from components.shooter import Shooter
 from components.vision import Vision
-from components.vision_coordinator import VisionCoordinator
 from controllers.shooter import ShooterController
 from utilities import game, positions
 from utilities.scalers import map_exponential
@@ -84,8 +83,6 @@ class MyRobot(magicbot.MagicRobot):
         ),
         Rotation3d(0, math.radians(-30), math.radians(-90)),
     )
-
-    vision_coordinator: VisionCoordinator
 
     def createObjects(self) -> None:
         self.gamepad = wpilib.XboxController(0)
