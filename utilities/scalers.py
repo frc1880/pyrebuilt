e1e89910f75b57b765260e573c066b6e2b30a123
James Ward <james@thedropbears.org.au>
2026-04-11 15:30:53 -0400

    Account for massive deadzone with xbox controller

--- utilities/scalers.py
+++ utilities/scalers.py
@@ -5,3 +5,14 @@ def map_exponential(value: float, base: float) -> float:
     """Takes a value in [-1,1] and maps it to an exponential curve."""
     assert base > 1
     return math.copysign((base ** abs(value) - 1) / (base - 1), value)
+
+
+def apply_deadzone(value: float, deadzone: float) -> float:
+    range = 1.0 - deadzone
+    scale = 1.0 / range
+    if value >= deadzone:
+        return (value - deadzone) * scale
+    elif value <= -deadzone:
+        return (value + deadzone) * scale
+    else:
+        return 0.0
