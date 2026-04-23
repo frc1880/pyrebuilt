6b4c937016cf78db883c875a708333ba9a3a2c3b
damian <payanodamian18@gmail.com>
2026-04-23 19:05:54 -0400

    Change convetions

--- components/vision_coordinator.py
+++ components/vision_coordinator.py
@@ -8,14 +8,14 @@ class VisionCoordinator:
 
     def execute(self) -> None:
         # Replaced camera check with a function
-        if self.onField():
+        if self.on_field():
             # One camera is initialised, so turn on multi tag flag for all
             self.red_vision._has_seen_multitag = True
             self.white_vision._has_seen_multitag = True
             self.shooter_vision._has_seen_multitag = True
 
     # Checks if one camera has seen a apriltag,, which would indicate if on field
-    def onField(self):
+    def on_field(self):
         return (
             self.red_vision._has_seen_multitag
             or self.white_vision._has_seen_multitag
